d3d8d20954f7b32ebb506b405d03133f8c33cc4c
Olumide Okedusi <odokedusi@sidmach.com>
2019-10-22 22:56:42 +0100

    Add api for CRUD operations

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const Thing = require("./models/thing");
+
 mongoose
   .connect(
     "mongodb+srv://openclassroom:openclassroom@openclassroomdb-1f0ft.mongodb.net/test?retryWrites=true&w=majority",
@@ -33,51 +35,87 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/stuff", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: "Thing created successfully!"
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
   });
+  thing
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: "Post saved successfully!"
+      });
+    })
+    .catch(error => {
+      res.status(400).json({
+        error: error
+      });
+    });
 });
 
 app.use("/api/stuff", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "My first thing",
-      description: "All of the info about my first thing",
-      imageUrl: "",
-      price: 4900,
-      userId: "qsomihvqios"
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "My second thing",
-      description: "All of the info about my second thing",
-      imageUrl: "",
-      price: 2900,
-      userId: "qsomihvqios"
-    }
-  ];
-  res.status(200).json(stuff);
+  Thing.find()
+    .then(things => {
+      res.status(200).json(things);
+    })
+    .catch(error => {
+      res.status(400).json({
+        error: error
+      });
+    });
 });
 
-// app.use((req, res, next) => {
-//   console.log("Request received!");
-//   next();
-// });
+app.get("/api/stuff/:id", (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id
+  })
+    .then(thing => {
+      res.status(200).json(thing);
+    })
+    .catch(error => {
+      res.status(404).json({
+        error: error
+      });
+    });
+});
 
-// app.use((req, res, next) => {
-//   res.status(201);
-//   next();
-// });
+app.put("/api/stuff/:id", (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  Thing.updateOne({ _id: req.params.id }, thing)
+    .then(() => {
+      res.status(201).json({
+        message: "Thing updated successfully!"
+      });
+    })
+    .catch(error => {
+      res.status(400).json({
+        error: error
+      });
+    });
+});
 
-// app.use((req, res, next) => {
-//   res.json({ message: "Your request was successful!" });
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log("Response sent successfully!");
-// });
+app.delete("/api/stuff/:id", (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: "Deleted!"
+      });
+    })
+    .catch(error => {
+      res.status(400).json({
+        error: error
+      });
+    });
+});
 
 module.exports = app;
